f108b9d619ca28d0b5c78bdcfce06fbf73008d5e
NataEditor <nako_al@mail.ru>
2025-08-02 14:29:30 +0300

    Add files via upload

---
+++ blogicum/pages/views.py
@@ -0,0 +1,12 @@
+# views.py
+from django.shortcuts import render
+
+
+def about(request):
+    temlate_name = 'pages/about.html'
+    return render(request, temlate_name)
+
+
+def rules(request):
+    template_name = 'pages/rules.html'
+    return render(request, template_name)
